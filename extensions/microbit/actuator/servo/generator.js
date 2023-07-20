5979deb69510e8021e9b5f7a398e190cfcc82a43
ArthurZheng <869705086@qq.com>
2023-07-20 16:33:24 +0800

    Normalized microbit servo plugins to use setups_ to set pin modes instead of libraries_

--- extensions/microbit/actuator/servo/generator.js
+++ extensions/microbit/actuator/servo/generator.js
@@ -8,7 +8,7 @@ function registerGenerators (Blockly) {
         const degree = Blockly.Python.valueToCode(this, 'degree', Blockly.Python.ORDER_ATOMIC);
 
         Blockly.Python.imports_.servo = 'from servo import Servo';
-        Blockly.Python.libraries_['servo_write_${port}'] = `sv${port} = Servo(pin${port})`;
+        Blockly.Python.setups_['servo_write_${port}'] = `sv${port} = Servo(pin${port})`;
 
         return `sv${port}.write_angle(${degree})\n`;
     };
